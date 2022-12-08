17064598da1d42e7a4f989aee6aabca9d3867804
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-12-08 05:16:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru_3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<char> sesli = new List<char>();
+            string cumle=Console.ReadLine();
+
+            for(int i = 0; i < cumle.Length; i++)
+            {
+                if ((cumle[i] == 'a' || cumle[i] == 'A') || (cumle[i] == 'e' || cumle[i] == 'E') || (cumle[i] == 'ı' || cumle[i] == 'I') || (cumle[i] == 'i' || cumle[i] == 'İ') || (cumle[i] == 'o' || cumle[i] == 'O') || (cumle[i] == 'ö' || cumle[i] == 'Ö') || (cumle[i] == 'u' || cumle[i] == 'U') || (cumle[i] == 'ü' || cumle[i] == 'Ü'))
+                    sesli.Add(cumle[i]);
+            }
+
+            sesli.Sort();
+            sesli.ForEach(i => Console.Write(i + " "));
+
+        }
+
+    }
+
+}
